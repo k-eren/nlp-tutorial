7816a7814b3362392c0474e899800f513414c574
k-eren <62838664+k-eren@users.noreply.github.com>
2024-01-08 20:31:48 +0800

    Update Word2Vec-Skipgram(Softmax).py

--- 1-2.Word2Vec/Word2Vec-Skipgram(Softmax).py
+++ 1-2.Word2Vec/Word2Vec-Skipgram(Softmax).py
@@ -9,12 +9,12 @@ import matplotlib.pyplot as plt
 def random_batch():
     random_inputs = []
     random_labels = []
-    random_index = np.random.choice(range(len(skip_grams)), batch_size, replace=False)
+    random_index = np.random.choice(range(len(skip_grams)), batch_size, replace=False)#在target数字中抽取batchsize个数字
 
     for i in random_index:
-        random_inputs.append(np.eye(voc_size)[skip_grams[i][0]])  # target
-        random_labels.append(skip_grams[i][1])  # context word
-
+        random_inputs.append(np.eye(voc_size)[skip_grams[i][0]])  # target输出的是对应的target索引位置为1，其余为0的向量
+        random_labels.append(skip_grams[i][1])  # context word 其对应的target
+        
     return random_inputs, random_labels
 
 # Model
@@ -29,31 +29,33 @@ class Word2Vec(nn.Module):
         # X : [batch_size, voc_size]
         hidden_layer = self.W(X) # hidden_layer : [batch_size, embedding_size]
         output_layer = self.WT(hidden_layer) # output_layer : [batch_size, voc_size]
+        #两层全连接层的计算
         return output_layer
 
 if __name__ == '__main__':
-    batch_size = 2 # mini-batch size
+    batch_size = 2 # mini-batch size 一个epoch里只有两个batch
     embedding_size = 2 # embedding size
 
     sentences = ["apple banana fruit", "banana orange fruit", "orange banana fruit",
                  "dog cat animal", "cat monkey animal", "monkey dog animal"]
 
-    word_sequence = " ".join(sentences).split()
+    word_sequence = " ".join(sentences).split()#分割成小句子
     word_list = " ".join(sentences).split()
-    word_list = list(set(word_list))
-    word_dict = {w: i for i, w in enumerate(word_list)}
-    voc_size = len(word_list)
+    word_list = list(set(word_list))#得到每个单词
+    word_dict = {w: i for i, w in enumerate(word_list)}#为每个单词编号
+    voc_size = len(word_list)#统计单词个数
 
     # Make skip gram of one size window
     skip_grams = []
-    for i in range(1, len(word_sequence) - 1):
+    for i in range(1, len(word_sequence) - 1):#不包含两端是因为最后的skipgram是[]target加上target这个词以及在句子中两端相邻的词
         target = word_dict[word_sequence[i]]
         context = [word_dict[word_sequence[i - 1]], word_dict[word_sequence[i + 1]]]
         for w in context:
             skip_grams.append([target, w])
+    #skipgram的结果是[target,左词] [target,右词]
 
     model = Word2Vec()
-
+    
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
@@ -78,5 +80,5 @@ if __name__ == '__main__':
         W, WT = model.parameters()
         x, y = W[0][i].item(), W[1][i].item()
         plt.scatter(x, y)
-        plt.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points', ha='right', va='bottom')
+        plt.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points', ha='right', va='bottom')#annotate为点添加注释标签
     plt.show()
